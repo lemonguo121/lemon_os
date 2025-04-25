c564535b818773df21ea6d6ea0fd80c21ab0443a
supermeguo <supermeguoguo@gmail>
2025-04-25 16:11:04 +0700

    1、爱听站接口文档

--- lib/http/HttpService.dart
+++ lib/http/HttpService.dart
@@ -13,7 +13,7 @@ class HttpService {
 //  •	获取歌词: 通过访问 https://music-plugings.onrender.com/lyric?id=12345（假设 bWhuZGt2bWg 是一个有效的歌曲 ID）
 //  •	获取排行榜: 通过访问 https://music-plugings.onrender.com/top-lists
 //  •	获取榜单详情: 通过访问 https://music-plugings.onrender.com/top-list-detail?id=new
-//  •	获取榜单详情: 通过访问 https://music-plugings.onrender.com/getMediaSource?id=bWhuZGt2bWg（假设 bWhuZGt2bWg 是一个有效的歌曲 ID）
+//  •	获取媒体信息: 通过访问 https://music-plugings.onrender.com/getMediaSource?id=bWhuZGt2bWg（假设 bWhuZGt2bWg 是一个有效的歌曲 ID）
 
 
   // final String baseUrl = "https://json.heimuer.xyz/api.php/provide/vod/";
