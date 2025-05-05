5c9956bbbee962d9fc50721d3a12eb857acd3fc7
supermeguo <supermeguoguo@gmail>
2025-05-05 18:51:09 +0700

    1、播放列表切换

--- lib/music/music_hot/HotDetailPage.dart
+++ lib/music/music_hot/HotDetailPage.dart
@@ -218,8 +218,8 @@ class _HotDetailPageState extends State<HotDetailPage> {
         onTap: () {
           playerController.playIndex.value = index;
           playerController.upDataSong(item);
-          var playRecordType = MusicSPManage.getCurrentPlayType();
-          MusicSPManage.saveCurrentPlayIndex(playRecordType.key, index);
+          // var playRecordType = MusicSPManage.getCurrentPlayType();
+          // MusicSPManage.saveCurrentPlayIndex(playRecordType.key, index);
         },
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.center,
